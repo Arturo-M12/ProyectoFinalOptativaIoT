d40767b8e10675da6c0288f89d29d3d3dae9415c
Arturo-M12 <71675966+Arturo-M12@users.noreply.github.com>
2021-11-20 20:09:16 -0600

    Servo 180 grados

---
+++ Pruebas de cada sistema individual/Servo180Grados/Servo180Grados.ino
@@ -0,0 +1,16 @@
+#include <Servo.h> //Incluir librería Servo de Arduino
+Servo servoElevador; //Declaración de la variable de elevador como tipo Servo
+void setup() {
+  // put your setup code here, to run once:
+  servoElevador.attach(16); //Se inicializa la variable en el puerto correspondiente
+  servoElevador.write(0); //Se le ajustan los grados iniciales
+}
+
+void loop() {
+  servoElevador.write(180); //Ajuste de grados
+  delay(5000);
+  servoElevador.write(90); //Ajuste de grados
+  delay(5000);
+  servoElevador.write(0); //Ajuste original de grados
+  delay(5000);
+}
