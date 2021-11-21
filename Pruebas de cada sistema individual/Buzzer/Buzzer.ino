33fcd5120947f3d2edd938c91ef73bca20cf030c
Arturo-M12 <71675966+Arturo-M12@users.noreply.github.com>
2021-11-20 20:19:44 -0600

    Buzzer

---
+++ Pruebas de cada sistema individual/Buzzer/Buzzer.ino
@@ -0,0 +1,13 @@
+int buzzerAlerta = 12; //Declaración e inicialización de la variable en el puerto correspondiente
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(buzzerAlerta,OUTPUT); //Se define al buzzer como output
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  tone(buzzerAlerta,1000); //Función tone para indicarle al buzzer que debe sonar a esa frecuencia
+  delay(1000);
+  noTone(buzzerAlerta); //Función noTone que hace que el sonido del buzzer se detenga
+  delay(5000);
+}
